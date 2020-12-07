3b907fa0372c70160212bb5d9ba19491a52e9bb4
toktar <renata.toktar@dsr-corporation.com>
2020-12-07 14:42:28 +0300

    VE-2229: add new methods to WalletActor, move some methods from PersistentActorConfigUtil to ConfigUtil
    Signed-off-by: toktar <renata.toktar@dsr-corporation.com>

--- verity/src/main/scala/com/evernym/verity/actor/wallet/WalletActor.scala
+++ verity/src/main/scala/com/evernym/verity/actor/wallet/WalletActor.scala
@@ -6,42 +6,70 @@ import java.time.temporal.ChronoUnit
 import akka.actor.Actor
 import com.evernym.verity.actor.ActorMessageClass
 import com.evernym.verity.actor.agent.SpanUtil.runWithInternalSpan
-import com.evernym.verity.config.AppConfigWrapper
-import com.evernym.verity.libindy.LibIndyWalletProvider
+import com.evernym.verity.config.{AppConfigWrapper, ConfigUtil}
+import com.evernym.verity.libindy.{LibIndyWalletExt, LibIndyWalletProvider}
 import com.evernym.verity.logging.LoggingUtil.getLoggerByClass
-import com.evernym.verity.vault.{WalletAccessParam, WalletExt, WalletProvider}
+import com.evernym.verity.vault.{WalletAccessParam, WalletAlreadyOpened, WalletDoesNotExist, WalletExt, WalletProvider}
 import com.typesafe.scalalogging.Logger
 
-case class CreateWallet(wap: WalletAccessParam) extends ActorMessageClass
-case class OpenWallet(wap: WalletAccessParam) extends ActorMessageClass
-case class CloseWallet(walletExt: WalletExt) extends ActorMessageClass
+import scala.concurrent.duration.Duration
 
-class WalletActor(walletProvider: WalletProvider = new LibIndyWalletProvider(AppConfigWrapper)) extends Actor {
+case class DeleteWallet() extends ActorMessageClass
+
+class WalletActor(walletProvider: WalletProvider = new LibIndyWalletProvider(AppConfigWrapper))
+  extends Actor {
 
   val logger: Logger = getLoggerByClass(classOf[WalletActor])
+  var walletExt: WalletExt = null
+  var wap: WalletAccessParam = null
+
+  def entityReceiveTimeout: Duration = ConfigUtil.getReceiveTimeout(
+    appConfig, defaultReceiveTimeoutInSeconds,
+    normalizedEntityCategoryName, normalizedEntityName, normalizedEntityId)
 
   def createWallet(wap: WalletAccessParam): Unit = {
-    runWithInternalSpan(s"createWallet", "runWithInternalSpan") {
+    runWithInternalSpan(s"createWallet", "WalletActor") {
       walletProvider.create(wap.walletName, wap.encryptionKey, wap.walletConfig)
     }
   }
 
-  def openWallet(implicit wap: WalletAccessParam): WalletExt = {
-    runWithInternalSpan(s"openWallet", "runWithInternalSpan") {
-      walletProvider.open(wap.walletName, wap.encryptionKey, wap.walletConfig)
+  def openWallet(wap: WalletAccessParam): Unit = {
+    runWithInternalSpan(s"openWallet", "WalletActor") {
+      try {
+        walletExt = walletProvider.open(wap.walletName, wap.encryptionKey, wap.walletConfig)
+      } catch {
+        case e: WalletAlreadyOpened => logger.debug(e.message)
+        case _: WalletDoesNotExist =>
+          walletProvider.createAndOpen(wap.walletName, wap.encryptionKey, wap.walletConfig)
+      }
     }
   }
 
-  def closeWallet(walletExt: WalletExt): Unit = {
-    runWithInternalSpan(s"closeWallet", "runWithInternalSpan") {
+  def closeWallet(): Unit = {
+    if (walletExt == null) {
+      logger.debug("WalletActor try to close not opened wallet")
+      return
+    }
+    runWithInternalSpan(s"closeWallet", "WalletActor") {
       walletProvider.close(walletExt)
     }
   }
 
   final override def receive = {
-    case CreateWallet(wap) => this.createWallet(wap)
-    case OpenWallet(wap) => this.openWallet(wap)
-    case CloseWallet(walletExt) => this.closeWallet(walletExt)
+    case DeleteWallet() => postStop()
+  }
+
+  @throws(classOf[Exception])
+  //#lifecycle-hooks
+  override def preStart(): Unit = {
+    context.setReceiveTimeout(entityReceiveTimeout)
+    openWallet(wap)
+  }
+
+  @throws(classOf[Exception])
+  //#lifecycle-hooks
+  override def postStop(): Unit = {
+    closeWallet()
   }
 
 }

ae36dae34bdb96f5c720b46489c5624c0f68bc89
Devin Fisher <devin.fisher@evernym.com>
2020-12-17 14:05:13 -0700

    pull for state change in DiscloseProof from ACK

--- integration-tests/src/test/scala/com/evernym/integrationtests/e2e/sdk/vcx/VcxPresentProof.scala
+++ integration-tests/src/test/scala/com/evernym/integrationtests/e2e/sdk/vcx/VcxPresentProof.scala
@@ -1,21 +1,21 @@
 package com.evernym.integrationtests.e2e.sdk.vcx
 
-import java.util.UUID
-
-import com.evernym.verity.protocol.engine.{DID, MsgFamily}
-import com.evernym.verity.protocol.protocols.presentproof.v_1_0.PresentProofMsgFamily
-import com.evernym.verity.sdk.protocols.presentproof.common.{Predicate, ProposedAttribute, ProposedPredicate}
-import org.json.JSONArray
-//import com.evernym.verity.protocol.protocols.vcx.presentProof.v_0_6.ProvingMsgFamily
+import akka.testkit.TestKit.awaitCond
 import com.evernym.integrationtests.e2e.msg.VcxGetMsg.vcxPayloadObject
 import com.evernym.integrationtests.e2e.sdk.UndefinedInterfaces.UndefinedPresentProof_1_0
 import com.evernym.integrationtests.e2e.sdk.vcx.VcxPresentProof.HolderProofInteraction
 import com.evernym.integrationtests.e2e.sdk.vcx.VcxSdkProvider.Interaction
 import com.evernym.sdk.vcx.proof.DisclosedProofApi
-import com.evernym.verity.sdk.protocols.presentproof.common.Attribute
+import com.evernym.verity.protocol.engine.{DID, MsgFamily}
+import com.evernym.verity.protocol.protocols.presentproof.v_1_0.PresentProofMsgFamily
+import com.evernym.verity.sdk.protocols.presentproof.common.{Attribute, Predicate, ProposedAttribute, ProposedPredicate}
 import com.evernym.verity.sdk.protocols.presentproof.v1_0.PresentProofV1_0
 import com.evernym.verity.sdk.utils.Context
-import org.json.JSONObject
+import org.json.{JSONArray, JSONObject}
+
+import java.util.UUID
+import scala.concurrent.duration.DurationInt
+import scala.language.postfixOps
 
 trait VcxPresentProof
   extends VcxHolds {
@@ -99,6 +99,16 @@ trait VcxPresentProof
         creds.put("attrs", selectedCredentials)
         DisclosedProofApi.proofGenerate(handle, creds.toString(), "{}").get()
         DisclosedProofApi.proofSend(handle, connHandle).get()
+
+        // Awaiting the ACK from the other side of the protocol.
+        awaitCond({
+          DisclosedProofApi.proofUpdateState(handle).get()
+          DisclosedProofApi.proofGetState(handle).get() == 4
+        },
+          5 seconds,
+          200 milliseconds
+        )
+
         DisclosedProofApi.proofRelease(handle)
       }
     }

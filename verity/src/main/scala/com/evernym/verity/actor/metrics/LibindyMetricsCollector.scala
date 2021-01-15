2eb5c121f5a68f4a314e20dfa95c8567c9b5418c
Alexey Shklyaev <alexey.shklyaev@evernym.com>
2021-01-15 17:44:28 +0300

    Added timeout for libindy metrics collection

--- verity/src/main/scala/com/evernym/verity/actor/metrics/LibindyMetricsCollector.scala
+++ verity/src/main/scala/com/evernym/verity/actor/metrics/LibindyMetricsCollector.scala
@@ -2,14 +2,17 @@ package com.evernym.verity.actor.metrics
 
 import akka.actor.Actor
 import com.evernym.verity.Exceptions
+import com.evernym.verity.ExecutionContextProvider.futureExecutionContext
+import com.evernym.verity.Main.agentMsgRouter.system
 import com.evernym.verity.actor.ActorMessage
 import com.evernym.verity.metrics.MetricsWriter
 import com.evernym.verity.util.JsonUtil.deserializeJsonStringToMap
 import com.evernym.verity.util.Util.logger
 import org.hyperledger.indy.sdk.metrics.Metrics
-import com.evernym.verity.ExecutionContextProvider.futureExecutionContext
 
 import scala.compat.java8.FutureConverters.{toScala => toFuture}
+import scala.concurrent.Future
+import scala.concurrent.duration.DurationInt
 import scala.util.{Failure, Success}
 
 class LibindyMetricsCollector extends Actor {
@@ -20,11 +23,13 @@ class LibindyMetricsCollector extends Actor {
 
   def collectLibindyMetrics(): Unit = {
     val replyTo = sender()
-    toFuture(Metrics.collectMetrics).onComplete {
+    val delayFuture = akka.pattern.after(10.seconds, using = system.scheduler)(Future.failed(new Exception("Metrics was not collected in 10s interval")))
+    val metricsFuture = toFuture(Metrics.collectMetrics)
+    Future.firstCompletedOf(Seq(metricsFuture, delayFuture)).onComplete {
       case Success(metrics) =>
         deserializeJsonStringToMap[String, Integer](metrics) foreach (
           metrics_item => MetricsWriter.gaugeApi.update(s"libindy_${metrics_item._1}", metrics_item._2.longValue())
-        )
+          )
         replyTo ! CollectLibindySuccess()
       case Failure(e) =>
         logger.warn(Exceptions.getStackTraceAsSingleLineString(e))

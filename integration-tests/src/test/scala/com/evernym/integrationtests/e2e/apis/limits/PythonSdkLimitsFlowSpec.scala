030c0b34419e2178e6a02631cec03cdfa7684baf
Alexey Shklyaev <alexey.shklyaev@evernym.com>
2021-04-30 19:06:45 +0300

    ve2415: fixed python and rest limits tests

--- integration-tests/src/test/scala/com/evernym/integrationtests/e2e/apis/limits/PythonSdkLimitsFlowSpec.scala
+++ integration-tests/src/test/scala/com/evernym/integrationtests/e2e/apis/limits/PythonSdkLimitsFlowSpec.scala
@@ -5,5 +5,5 @@ import com.evernym.integrationtests.e2e.env.IntegrationTestEnv
 
 class PythonSdkLimitsFlowSpec extends LimitsFlowSpec {
   override def specifySdkType(env: IntegrationTestEnv): IntegrationTestEnv =
-    SdkFlowSpec.specifySdkForType("python", "0.4.5.54cc6088", env)
+    SdkFlowSpec.specifySdkForType("python", "0.4.8.fceed2a1", env)
 }

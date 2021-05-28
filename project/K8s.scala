5e39088668c9c09d8f4a89864c0ea2de473555db
Nikita Khateev <nikita.khateev@dsr-corporation.com>
2021-05-28 19:11:15 +0300

    VE-2409 -- fix typo

--- project/K8s.scala
+++ project/K8s.scala
@@ -12,7 +12,7 @@ import java.nio.file.Path
 import scala.collection.JavaConverters._
 
 object K8sTasks {
-  def init(addedJars: Seq[String], libindyVer: Stringg) = {
+  def init(addedJars: Seq[String], libindyVer: String) = {
     Seq(
       k8sDockerPackageDir := { target.value / "docker" },
       k8sConfigGenDir := { k8sDockerPackageDir.value / "configuration" },

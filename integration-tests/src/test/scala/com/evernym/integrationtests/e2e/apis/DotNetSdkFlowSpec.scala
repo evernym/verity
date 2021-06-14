0d914c3665e6cefe534dc16b65b2a85698000eb0
Devin Fisher <devin.fisher@evernym.com>
2021-06-14 15:17:15 -0600

    switch to use public development repos for verity-sdk (gitlab repos)

--- integration-tests/src/test/scala/com/evernym/integrationtests/e2e/apis/DotNetSdkFlowSpec.scala
+++ integration-tests/src/test/scala/com/evernym/integrationtests/e2e/apis/DotNetSdkFlowSpec.scala
@@ -6,5 +6,5 @@ import com.evernym.integrationtests.e2e.tag.annotation.Integration
 @Integration
 class DotNetSdkFlowSpec extends SdkFlowSpec {
   override def specifySdkType(env: IntegrationTestEnv): IntegrationTestEnv =
-    SdkFlowSpec.specifySdkForType("dotnet", "0.4.7-6e248981", env)
+    SdkFlowSpec.specifySdkForType("dotnet", "0.4.8-bf95110f", env)
 }
--- integration-tests/src/test/scala/com/evernym/integrationtests/e2e/apis/NodeSdkFlowSpec.scala
+++ integration-tests/src/test/scala/com/evernym/integrationtests/e2e/apis/NodeSdkFlowSpec.scala
@@ -6,5 +6,5 @@ import com.evernym.integrationtests.e2e.tag.annotation.Integration
 @Integration
 class NodeSdkFlowSpec extends SdkFlowSpec {
   override def specifySdkType(env: IntegrationTestEnv): IntegrationTestEnv =
-    SdkFlowSpec.specifySdkForType("node", "0.4.5-705fb23c", env)
+    SdkFlowSpec.specifySdkForType("node", "0.4.8-d248cd7a", env)
 }

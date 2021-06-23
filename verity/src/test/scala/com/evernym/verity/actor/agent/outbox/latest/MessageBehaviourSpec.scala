fed4459e21b496a7084514340497b8c74480372b
Rajesh Kalaria <rajesh.kalaria@evernym.com>
2021-06-23 11:19:36 +0530

    continuous refactoring

--- verity/src/test/scala/com/evernym/verity/actor/agent/outbox/latest/MessageBehaviourSpec.scala
+++ verity/src/test/scala/com/evernym/verity/actor/agent/outbox/latest/MessageBehaviourSpec.scala
@@ -2,81 +2,129 @@ package com.evernym.verity.actor.agent.outbox.latest
 
 import com.evernym.verity.testkit.BasicSpec
 
+
 class MessageBehaviourSpec
   extends BasicSpec {
 
   "Message behaviour" - {
 
-    "when not yet started" - {
-      "and sent Get command" - {
-        "should respond with MsgNotYetAdded message" in {
+    "in Uninitialized state" - {
+
+      "when sent Get command" - {
+        "should respond with MsgNotAdded message" in {
+          pending
+        }
+      }
+
+      //what would 'Add' command will contain (message detail, data retention policy)?
+      "when sent Add command" - {
+        "should respond with Acknowledgement" in {
           pending
         }
       }
     }
 
-    "when started" - {
+    "in Initialized state" - {
 
-      "in Empty state" - {
-        //what would 'Add' command will contain (message detail, data retention policy)?
-        "when sent Add command" - {
-          "should respond with Acknowledgment" in {
-            pending
-          }
-        }
-        //this confirms that Message behaviour as part of previous
-        // Add command, did store the payload to external storage (mock s3 etc)
-        "when checked external storage for payload" - {
-          "it should be found" in {
-            pending
-          }
+      //this confirms that "Message behaviour" as part of previous
+      // Add command stored the payload to external storage (mock s3 etc)
+      "when checked external storage for payload" - {
+        "it should be found" in {
+          pending
         }
       }
 
-      "in Added state" - {
-        "when sent Add command" - {
-          "should respond with MsgAlreadyAdded message" in {
-            pending
-          }
-        }
-
-        "when sent Get command" - {
-          "should respond with Message" - {
-            pending
-          }
-        }
-
-        "when deleted the payload from external storage (s3 mock etc)" - {
-          "should be successful" - {
-            pending
-          }
-        }
-
-        //this confirms that MessageBehaviour has the payload in it's in-memory state
-        "when send Get command (post payload deletion from external storage)" - {
-          "should still respond with Message along with payload" - {
-            pending
-          }
-        }
-
-        "when sent Stop command" - {
-          "should be stopped" in {
-            pending
-          }
+      "when sent Add command" - {
+        "should respond with MsgAlreadyAdded message" in {
+          pending
         }
       }
-    }
 
-    "when stopped (post message added)" - {
-
-      //this confirms that actor recovery works fine
-      "and sent Get command" - {
+      "when sent Get command" - {
         "should respond with Message" in {
           pending
         }
       }
-    }
 
+      "when deleted the payload from external storage (s3 mock etc)" - {
+        "should be successful" in {
+          pending
+        }
+      }
+
+      //this confirms that MessageBehaviour has the payload in it's in-memory state
+      "when send Get command (post payload deletion from external storage)" - {
+        "should still respond with Message along with payload" in {
+          pending
+        }
+      }
+
+      "when sent Stop command" - {
+        "should be stopped" in {
+          pending
+        }
+      }
+
+      //this confirms that actor recovery works fine
+      "when sent Get command" - {
+        "should respond with Message" in {
+          pending
+        }
+      }
+
+      "when sent GetDeliveryStatus(outbox-id) command" - {
+        "should respond with NoDeliveryStatusFound" in {
+          pending
+        }
+      }
+
+      //with status=undelivered and detail=Detail(com-method-id-1, 'success_count=0,failed_count=1,message=error-message')
+      "when sent AddDeliveryStatus(outbox-id-1, undelivered, detail) command" - {
+        "should respond with Acknowledgement" in {
+          pending
+        }
+      }
+
+      "when sent GetDeliveryStatus(outbox-id) command" - {
+        "should respond with appropriate DeliveryStatus" in {
+          /*
+            DeliveryStatus(
+              undelivered,
+              Map(
+                'com-method-id-1' -> List(
+                    Detail(timestamp, 'success_count=0,failed_count=1,message=error-message')
+                  )
+              )
+           )
+           */
+          pending
+        }
+      }
+
+      //with status=delivered and detail=Detail(com-method-id-1, 'success_count=1,failed_count=1')
+      "when sent AddDeliveryStatus(outbox-id-1, undelivered, detail) command" - {
+        "should respond with Acknowledgement" in {
+          pending
+        }
+      }
+
+      "when sent GetDeliveryStatus(outbox-id) command" - {
+        "should respond with appropriate DeliveryStatus" in {
+          /*
+            DeliveryStatus(
+              delivered,
+              Map(
+                'com-method-id-1' -> List(
+                    Detail(timestamp, 'success_count=0,failed_count=1,message=error-message'),
+                    Detail(timestamp, 'success_count=1,failed_count=1'),
+                  )
+              )
+           )
+           */
+          pending
+        }
+      }
+    }
   }
 }
 
--- verity/src/test/scala/com/evernym/verity/actor/agent/outbox/latest/OutboxBehaviourSpec.scala
+++ verity/src/test/scala/com/evernym/verity/actor/agent/outbox/latest/OutboxBehaviourSpec.scala
@@ -1,46 +1,148 @@
 package com.evernym.verity.actor.agent.outbox.latest
 
 import com.evernym.verity.testkit.BasicSpec
+import org.scalatest.concurrent.Eventually
+import org.scalatest.time.{Millis, Seconds, Span}
+
+//the outbox 'entity-id' will be created by concatenating 'relationship-id' and 'destination-id' and
+// either:
+//  * when relationship actors (UserAgent and/or UserAgentPairwise) start, they also starts all required outbox actors
+// or:
+//  * the outbox actors when started, extracts the relationshipId from entity id and
+//    get necessary details from that relationship actor
+
+
+// so an identity owner can have these outbox actors:
+// * For Legacy APIs
+//   * In scope of 'self-relationship':
+//        'Outbox-selfRelId-default'            (for CAS/EAS/VAS)
+//   * In scope of 'pairwise-relationship'
+//        'Outbox-selfRelId-default'            (for CAS/EAS/VAS)
+//        'Outbox-theirPairwiseRelId-default'   (for CAS/EAS/VAS)
+
+// * For New APIs
+//   * In scope of 'self-relationship':
+//        'Outbox-selfRelId-dest-1'             (for CAS/EAS/VAS)
+//   * In scope of 'pairwise-relationship'
+//        'Outbox-selfRelId-dest-1'             (for CAS/EAS/VAS)
+//        'Outbox-theirPairwiseRelId-default'   (for CAS/EAS/VAS)
+
 
 class OutboxBehaviourSpec
-  extends BasicSpec {
+  extends BasicSpec
+    with Eventually {
 
   "Outbox behaviour" - {
 
-    "when not yet started" - {
-      "and sent GetMsgs command" - {
-        "should respond with Msgs with empty list" in {
+    "in Uninitialized state" - {
+
+      "when sent GetDeliveryChannels command" - {
+        "should respond with DeliveryChannelsNotAdded" in {
+          pending
+        }
+      }
+
+      //relationshipId will be used by Outbox to reach back to the
+      // specific relationship actor for various purposes
+      // (fetching/updating com methods, to pack message etc)
+      "when sent Initialize(relationshipId, DeliveryChannels(empty or initial com methods)) command" - {
+        "should respond with Acknowledgement" in {
           pending
         }
       }
     }
 
-    "when started" - {
+    //Initialized state means it has sufficient information to be functional
+    "in Initialized state" - {
 
-      "in Empty state" - {
-        //what would 'AddMsg' command will contain (msgId, ...)?
-        "when sent AddMsg command" - {
-          "should respond with Acknowledgment" in {
+      "when sent GetDeliveryChannels command" - {
+        "should (eventually) respond with DeliveryChannels (with initial com methods)" in {
+          eventually(timeout(Span(5, Seconds)), interval(Span(50, Millis))) {
             pending
           }
         }
       }
 
-      "in Added state" - {
-        "when sent AddMsg command again" - {
-          "should respond with MsgAlreadyAdded message" in {
-            pending
-          }
+      //need to make sure if given delivery channels and
+      // stored delivery channels are same then it doesn't persist unnecessary event.
+      "when sent UpdateDeliveryChannels(com-method-1, com-method-2) command" - {
+        "should respond with Acknowledgement" in {
+          pending
         }
+      }
 
-        "when sent GetMsgs command" - {
-          "should respond with Msgs with 1 message" in {
-            pending
-          }
+      "when sent GetDeliveryChannels command" - {
+        "should respond with DeliveryChannels(com-method-1, com-method-2)" in {
+          pending
+        }
+      }
+
+      "when sent GetMsgs command" - {
+        "should respond with Messages(empty list)" in {
+          pending
+        }
+      }
+
+      //with no packaging protocol (for rest api webhook)
+      "when sent AddMsg(msg-1, ...) command" - {
+        "should respond with Acknowledgement message" in {
+          pending
+        }
+      }
+
+      "when sent GetMsgs command" - {
+        "should respond with Messages(msg-1)" in {
+          pending
+        }
+      }
+
+      "when sent same AddMsg(msg-1, ...) command again" - {
+        "should respond with MsgAlreadyAdded message" in {
+          pending
+        }
+      }
+
+      //with legacy packaging protocol
+      "when sent different AddMsg(msg-2, ...) command" - {
+        "should respond with Acknowledgement message" in {
+          pending
+        }
+      }
+
+      "when sent GetMsgs command" - {
+        "should respond with Messages(msg-1, msg-2)" in {
+          pending
+        }
+      }
+
+      //with did-com-v1 packaging protocol
+      "when sent another AddMsg(msg-3, ...) command" - {
+        "should respond with Acknowledgement message" in {
+          pending
+        }
+      }
+
+      "when sent GetMsgs command" - {
+        "should respond with Messages(msg-1, msg-2, msg-3)" in {
+          pending
+        }
+      }
+
+      //delivered (successfully or failed with exhausted retries)
+      // messages will be removed from the state
+      "when checking status in outbox actor" - {
+        "eventually those messages should disappear" in {
+          pending
+        }
+      }
+
+      //NOTE: this is against Message actor (and not the Outbox actor)
+      "when checking the Message actors" - {
+        "there should be delivery status found for this outbox" in {
+          pending
         }
       }
     }
-
   }
 
 }

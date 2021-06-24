725fe0919a7ed95dfdc3e0185af7c5dea953758b
Rajesh Kalaria <rajesh.kalaria@evernym.com>
2021-06-24 14:18:05 +0530

    fixed relationship service interface

--- verity/src/test/scala/com/evernym/verity/actor/agent/outbox/latest/ImplementationDetails.md
+++ verity/src/test/scala/com/evernym/verity/actor/agent/outbox/latest/ImplementationDetails.md
@@ -1,12 +1,13 @@
 ## Integration changes
 
 ### DeliveryMechanism management
-**RelationshipActors**
+**RelationshipActors (UserAgent and UserAgentPairwise)**
 
-* Has endpoints (for different agents) in respective DIDDocs. 
+* Has communication method details:
+    ActorState -> relationship -> DIDDoc -> Endpoints. 
 * As of today we only support one destination, in future when we have to support multiple destinations,
   it may/will require some changes in the relationship data model.
-    
+
 ### Outgoing Message integration
 **ActorProtocolContainer**
 * the synchronous response message:
@@ -22,33 +23,46 @@
 **AgentMsgProcessor**
 * for outgoing protocol message received from other domain
   `OutgoingRouterService.sendMsg(fromParticipantId, toParticipantId, jsonMsg, metadata, binaryProtocol)`
+* will get rid of all other outgoing message handling logic
 
 ### OutgoingRouterService
 sendMsg(fromParticipantId, toParticipantId, jsonMsg, metadata, binaryProtocol): Unit
 
 ### OutgoingRouterService Implementation
 sendMsg(fromParticipantId, toParticipantId, jsonMsg, metadata, binaryProtocol): Unit
-  - creates a new `MessageBehaviour` and sends it `AddMsg(jsonMsg, metadata, data-retention-policy)`
+  - Assumes that the jsonMsg already contains required fields (@type, ~thread etc)
   - RelationshipService.getOutboxIds(fromParticipantId) map { outboxIds =>
-      for each outboxIds ->
-        sends a message `AddMsg(msgId)` to the **sharded** `OutboxBehavior`
-  }
+        - store payload to external storage (S3)
+        - creates a new `MessageBehaviour` (sharded persistent entity) and sends it
+            `AddMsg(metadata, data-retention-policy, payload_location, outboxIds)`
+        for each outboxIds ->
+            sends a message `AddMsg(msgId)` to the **sharded** `OutboxBehavior`
+    }
 
 ### Message behavior
-* The message payload will be always a json message with all the required data in it (@type, ~thread etc)
-* The message payload will be encrypted before it gets stored in external storage
+* Stores message and activity level data
+* After each activity it will check if the message is delivered (or permanently failed) to all attached outboxes
+  and in that case it will delete the payload from external storage (S3) 
+  and log that activity as well.
 
 ### Outbox behaviour
 * 'entity-id' will be created by concatenating 'relationship-id' and 'destination-id'.
-* when started, calls `RelationshipService.getOutboxParam(relationship-id)`
+* when started, calls `RelationshipService.getOutboxParam(relationship-id, destination-id)`
 * when receives OutboxParam, then only it starts processing message deliveries.
+* it is supposed to create a **child** `ReadOnlyMessage` behaviour to get Message related data.
+* after any activity on message, it will send appropriate command to `Message` behaviour only.
+
+### ReadOnlyMessage behaviour (non-persistent)
+* Loads event (first event only) for given PersistenceId
+* Will be mainly used to get the message payload from external location (S3)
 
 ### RelationshipService
-  getOutboxParam(forRelationshipId): Future[OutboxParam]
+  getOutboxParam(forRelationshipId, destination-id): Future[OutboxParam]
 
 ### RelationshipServiceImpl
-  getOutboxParam(forRelationshipId): Future[OutboxParam]
-    asks from relationship actor and returns it back 
+  getOutboxParam(forRelationshipId, destination-id): Future[OutboxParam]
+    asks from relationship actor and returns requested information back 
+    (Destination(com-methods), walletId)
 
 ### Possible Outboxes (for an identity owner)
 **For Legacy APIs**
@@ -57,8 +71,6 @@ sendMsg(fromParticipantId, toParticipantId, jsonMsg, metadata, binaryProtocol): 
 * Outbox/theirPairwiseRelId2-default   (for CAS/EAS/VAS)
 * ...
 * ...
-
-<br>
 
 **For New APIs**
 * Outbox/selfRelId-<dest-1>            (for CAS/EAS/VAS)
--- verity/src/test/scala/com/evernym/verity/actor/agent/outbox/latest/OutboxBehaviourSpec.scala
+++ verity/src/test/scala/com/evernym/verity/actor/agent/outbox/latest/OutboxBehaviourSpec.scala
@@ -2,7 +2,6 @@ package com.evernym.verity.actor.agent.outbox.latest
 
 import com.evernym.verity.testkit.BasicSpec
 import org.scalatest.concurrent.Eventually
-import org.scalatest.time.{Millis, Seconds, Span}
 
 class OutboxBehaviourSpec
   extends BasicSpec
@@ -10,83 +9,17 @@ class OutboxBehaviourSpec
 
   "Outbox behaviour" - {
 
-    "in Uninitialized state" - {
-
-      "when sent GetDeliveryChannels command" - {
-        "should respond with DeliveryChannelsNotAdded" in {
-          pending
-        }
-      }
-
-      //relationshipId will be used by Outbox to reach back to the
-      // specific relationship actor for various purposes
-      // (fetching/updating com methods, to pack message etc)
-      "when sent Initialize(relationshipId, DeliveryChannels(empty or initial com methods)) command" - {
-        "should respond with Acknowledgement" in {
-          pending
-        }
+    "when started for the first time" - {
+      "should fetch required information from relationship actor" in {
+        //relationshipId and destinationId will be used by Outbox
+        // to reach back to the specific relationship actor to
+        // fetch required information (destination specific com methods and wallet id etc)
+        // and keep it in memory only
+        pending
       }
     }
 
-    //Initialized state means it has sufficient information to be functional
-    "in Initialized state" - {
-
-      "when sent GetDeliveryChannels command" - {
-        "should (eventually) respond with DeliveryChannels (with initial com methods)" in {
-          eventually(timeout(Span(5, Seconds)), interval(Span(50, Millis))) {
-            pending
-          }
-        }
-      }
-
-      //make sure Outbox validates push com method delivery channel
-      "when sent invalid UpdateDeliveryChannels(DeliveryChannel(push-token)) command" - {
-        "should respond with appropriate Error message" in {
-          pending
-        }
-      }
-
-      //make sure Outbox validates sponsor push delivery channel
-      "when sent invalid UpdateDeliveryChannels(DeliveryChannel(sponsor-push)) command" - {
-        "should respond with appropriate Error message" in {
-          pending
-        }
-      }
-
-      //make sure Outbox validates webhook delivery channel
-      "when sent invalid UpdateDeliveryChannels(DeliveryChannel(webhook)) command" - {
-        "should respond with appropriate Error message" in {
-          pending
-        }
-      }
-
-      //make sure Outbox validates forward push delivery channel
-      "when sent invalid UpdateDeliveryChannels(DeliveryChannel(fwd-push)) command" - {
-        "should respond with appropriate Error message" in {
-          pending
-        }
-      }
-
-      //update all com methods
-      "when sent UpdateDeliveryChannels command" - {
-        "should respond with Acknowledgement" in {
-          pending
-        }
-      }
-
-      //need to make sure if given delivery channels and
-      // stored delivery channels are same then it doesn't persist unnecessary event.
-      "when sent same (as above) UpdateDeliveryChannels command" - {
-        "should respond with Acknowledgement" in {
-          pending
-        }
-      }
-
-      "when sent GetDeliveryChannels command" - {
-        "should respond with DeliveryChannels(push, sponsor-push, webhook, fwd-push)" in {
-          pending
-        }
-      }
+    "in already started state" - {
 
       "when sent GetMsgs command" - {
         "should respond with Messages(empty list)" in {
@@ -94,9 +27,9 @@ class OutboxBehaviourSpec
         }
       }
 
-      //with no packaging protocol (for rest api webhook)
+      //no packaging protocol (for rest api webhook)
       "when sent AddMsg(msg-1, ...) command" - {
-        "should respond with Acknowledgement message" in {
+        "should be successful" in {
           pending
         }
       }
@@ -115,7 +48,7 @@ class OutboxBehaviourSpec
 
       //with legacy packaging protocol
       "when sent different AddMsg(msg-2, ...) command" - {
-        "should respond with Acknowledgement message" in {
+        "should be successful" in {
           pending
         }
       }
@@ -128,7 +61,7 @@ class OutboxBehaviourSpec
 
       //with did-com-v1 packaging protocol
       "when sent another AddMsg(msg-3, ...) command" - {
-        "should respond with Acknowledgement message" in {
+        "should be successful" in {
           pending
         }
       }
@@ -139,10 +72,10 @@ class OutboxBehaviourSpec
         }
       }
 
-      //delivered (successfully or failed with exhausted retries) or acknowledged
-      // messages will be removed from the state
-      "when checking status in outbox actor" - {
+      "when periodically checking outbox status" - {
         "eventually those messages should disappear" in {
+          //delivered (successfully or failed with exhausted retries) or acknowledged
+          // messages will be removed from the state
           pending
         }
       }
@@ -155,7 +88,7 @@ class OutboxBehaviourSpec
       }
 
       "when sent lots of AddMsg commands" - {
-        "should respond with Acknowledgement message for each of them" in {
+        "should be successful" in {
           pending
         }
       }
@@ -165,7 +98,7 @@ class OutboxBehaviourSpec
           (1 to 10).foreach { _ =>
             //send GetMsgs command
             // it should respond with
-            //  - only undelivered messages
+            //  - only undelivered and/or unacknowledged messages
             //  - they should be in their creation/arrival order
             //  - they may get limited by overall "payload" size or by "total number of messages"
             pending

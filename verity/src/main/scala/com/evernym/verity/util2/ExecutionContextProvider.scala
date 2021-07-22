57985bbbf44b4b364c2e00912e5aca0efbec2f1c
Rajesh Kalaria <rajesh.kalaria@evernym.com>
2021-07-22 17:49:40 +0530

    removed execution context instrumentation

--- verity/src/main/scala/com/evernym/verity/util2/ExecutionContextProvider.scala
+++ verity/src/main/scala/com/evernym/verity/util2/ExecutionContextProvider.scala
@@ -2,10 +2,6 @@ package com.evernym.verity.util2
 
 import com.evernym.verity.config.AppConfigWrapper
 import com.evernym.verity.config.CommonConfig.{VERITY_DEFAULT_FUTURE_THREAD_POOL_SIZE, VERITY_WALLET_FUTURE_THREAD_POOL_SIZE}
-
-//TODO: do we want to abstract this kamon instrumentation api as well as we did for metrics api?
-import kamon.instrumentation.executor.ExecutorInstrumentation
-
 import java.util.concurrent.Executors
 import scala.concurrent.ExecutionContext
 import scala.concurrent.ExecutionContext.Implicits
@@ -22,19 +18,15 @@ object ExecutionContextProvider {
    * custom thread pool executor
    */
   implicit val futureExecutionContext: ExecutionContext =
-    ExecutorInstrumentation.instrumentExecutionContext(
-      defaultFutureThreadPoolSize match {
-        case Some(size) => ExecutionContext.fromExecutor(Executors.newFixedThreadPool(size))
-        case _ => Implicits.global
-      },
-      "future-thread-executor")
+    defaultFutureThreadPoolSize match {
+      case Some(size) => ExecutionContext.fromExecutor(Executors.newFixedThreadPool(size))
+      case _ => Implicits.global
+    }
 
   implicit val walletFutureExecutionContext: ExecutionContext =
-    ExecutorInstrumentation.instrumentExecutionContext(
-      walletFutureThreadPoolSize match {
-        case Some(size) => ExecutionContext.fromExecutor(Executors.newFixedThreadPool(size))
-        case _ => futureExecutionContext
-      },
-      "wallet-thread-executor")
+    walletFutureThreadPoolSize match {
+      case Some(size) => ExecutionContext.fromExecutor(Executors.newFixedThreadPool(size))
+      case _ => futureExecutionContext
+    }
 
 }

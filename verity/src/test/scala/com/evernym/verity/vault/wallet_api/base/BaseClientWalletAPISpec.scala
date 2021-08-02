fe5a1cbf15d78b7c03dd4b76fd4465a8ec58f9a4
Alexey Shklyaev <alexey.shklyaev@evernym.com>
2021-08-02 17:00:06 +0300

    VE-2717: switched to sequental operations for wallet actor

--- verity/src/test/scala/com/evernym/verity/vault/wallet_api/base/BaseClientWalletAPISpec.scala
+++ verity/src/test/scala/com/evernym/verity/vault/wallet_api/base/BaseClientWalletAPISpec.scala
@@ -1,17 +1,16 @@
 package com.evernym.verity.vault.wallet_api.base
 
-import java.util.UUID
 import akka.actor.ActorRef
 import com.evernym.verity.actor.testkit.actor.ProvidesMockPlatform
 import com.evernym.verity.actor.testkit.{ActorSpec, CommonSpecUtil}
 import com.evernym.verity.actor.wallet._
 import com.evernym.verity.testkit.{BasicSpec, HasTestWalletAPI}
-import com.evernym.verity.util2.ExecutionContextProvider
-import com.evernym.verity.vault.wallet_api.WalletAPI
-import com.evernym.verity.vault.WalletAPIParam
 import com.evernym.verity.util2.HasExecutionContextProvider
+import com.evernym.verity.vault.WalletAPIParam
+import com.evernym.verity.vault.wallet_api.WalletAPI
 import com.typesafe.config.{Config, ConfigFactory}
 
+import java.util.UUID
 import java.util.concurrent.atomic.AtomicInteger
 import scala.concurrent.{ExecutionContext, Future}
 
@@ -29,18 +28,21 @@ trait ClientWalletAPISpecBase
   // if corresponding code is enabled in 'HasThreadStarvationDetector'
   // keep overriding in implementing class as needed
   implicit val testCodeExecutionContext: ExecutionContext = futureExecutionContext
-    //comment above and uncomment/modify below to use custom thread pool
-    //ExecutionContext.fromExecutor(Executors.newFixedThreadPool(10))
+  //comment above and uncomment/modify below to use custom thread pool
+  //ExecutionContext.fromExecutor(Executors.newFixedThreadPool(10))
 
   ////NOTE: enable below function call to check for thread starvation for 'testCodeExecutionContext'
   //checkThreadStarvationFor(testCodeExecutionContext)
 
   def totalUsers: Int
+
   def startUserWalletSetupWithAsyncAPI(): Unit
+
   def waitForAllResponses(): Unit
 
   val successResp = new AtomicInteger(0)
   val failedResp = new AtomicInteger(0)
+
   def totalRespCount: Int = successResp.get() + failedResp.get()
 
   lazy val libIndyMetricsCollector: ActorRef = platform.libIndyMetricsCollector
@@ -69,12 +71,14 @@ trait UserWalletSetupHelper {
       _ <- walletAPI.executeAsync[WalletCreated.type](CreateWallet())
     ) yield {
       val theirDidPair = CommonSpecUtil.generateNewDid()
-      val fut1 = walletAPI.executeAsync[NewKeyCreated](CreateNewKey())
-      val fut2 =
-        walletAPI.executeAsync[TheirKeyStored](StoreTheirKey(theirDidPair.DID, theirDidPair.verKey)).map { _ =>
-          walletAPI.executeAsync[GetVerKeyOptResp](GetVerKeyOpt(theirDidPair.DID))
-        }
-      Future.sequence(Seq(fut1, fut2))
+      Future.sequence(
+        Seq(
+          walletAPI.executeAsync[NewKeyCreated](CreateNewKey()),
+          walletAPI.executeAsync[TheirKeyStored](StoreTheirKey(theirDidPair.DID, theirDidPair.verKey)).map { _ =>
+            walletAPI.executeAsync[GetVerKeyOptResp](GetVerKeyOpt(theirDidPair.DID))
+          }
+        )
+      )
     }
     result.flatten
   }

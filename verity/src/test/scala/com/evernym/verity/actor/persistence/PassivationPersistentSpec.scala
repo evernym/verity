69f1cfef658c901d3822f5ce83f2298f6bf24036
Nikita Khateev <nikita.khateev@dsr-corporation.com>
2021-08-23 19:22:09 +0300

    VE-2833 -- test finalization

--- verity/src/test/scala/com/evernym/verity/actor/persistence/PassivationPersistentSpec.scala
+++ verity/src/test/scala/com/evernym/verity/actor/persistence/PassivationPersistentSpec.scala
@@ -1,50 +1,61 @@
 package com.evernym.verity.actor.persistence
 
+import scala.concurrent.duration.Duration
 import java.util.concurrent.TimeUnit
 
-import akka.actor.{Actor, ActorRef, ActorSystem, Props}
-import akka.cluster.sharding.ShardRegion.{CurrentShardRegionState, ExtractEntityId, ExtractShardId, GetShardRegionState}
-import akka.cluster.sharding.{ClusterSharding, ClusterShardingSettings}
-import akka.testkit.{ImplicitSender, TestKit, TestProbe}
-import com.evernym.verity.actor.{ForIdentifier, ShardIdExtractor, ShardUtil}
+import akka.actor.{Actor, Props}
+import akka.cluster.sharding.ShardRegion.{CurrentShardRegionState, GetShardRegionState}
+import akka.cluster.sharding.ClusterSharding
+import akka.testkit.{ImplicitSender, TestKitBase}
+import com.evernym.verity.actor.testkit.HasBasicActorSystem
+import com.evernym.verity.actor.{ForIdentifier, ShardUtil}
 import com.evernym.verity.testkit.BasicSpec
-import com.evernym.verity.util.TestExecutionContextProvider
 
 import scala.concurrent.duration.FiniteDuration
 
 class PassivationPersistentSpec
-  extends TestKit(ActorSystem("test"))
-    with BasicSpec
-    with ImplicitSender {
-  val sharding = ClusterSharding(system)
-  val props = Props(new TestActor())
-  val extractShardId: ExtractShardId = ShardUtil.forIdentifierShardIdExtractor(ShardIdExtractor(TestExecutionContextProvider.testAppConfig, "test"))
-  val extractEntityId: ExtractEntityId = ShardUtil.forIdentifierEntityIdExtractor
-  val clusterShardingSettings = ClusterShardingSettings(system).withPassivateIdleAfter(FiniteDuration(30, TimeUnit.SECONDS))
-  val shardingRegion = sharding.start("test", props, clusterShardingSettings, extractEntityId, extractShardId)
+  extends BasicSpec
+    with TestKitBase
+    with HasBasicActorSystem
+    with ImplicitSender
+    with ShardUtil {
+  val region = createNonPersistentRegion(
+    "test",
+    Props[TestActor](),
+    passivateIdleEntityAfter = Some(FiniteDuration(20, TimeUnit.SECONDS))
+  )(system)
 
   "PassivationPersistentSpec" - {
     "when timeout hits actor should be passivated" in {
-      val probe = TestProbe()
-      shardingRegion ! ForIdentifier("123", TestMessage("test", probe.ref))
-      probe.expectMsg(TestReply("test"))
+      val shardingRegion = ClusterSharding(system).shardRegion("test")
+      shardingRegion ! ForIdentifier("123", TestMessage("test"))
+      expectMsg(TestReply("test"))
       shardingRegion ! GetShardRegionState
       val clusterState = receiveOne(FiniteDuration(10, TimeUnit.SECONDS))
       clusterState should not be null
-      System.out.print(clusterState.asInstanceOf[CurrentShardRegionState].shards.toString())
+      var state: CurrentShardRegionState = clusterState.asInstanceOf[CurrentShardRegionState]
+      state.shards.foreach(shard => {
+        shard.entityIds.size shouldBe 1
+      })
+      Thread.sleep(30000)
+      awaitCond({
+        shardingRegion ! GetShardRegionState
+        val clusterStateAfterTimeout = receiveOne(FiniteDuration(10, TimeUnit.SECONDS))
+        state = clusterStateAfterTimeout.asInstanceOf[CurrentShardRegionState]
+        state.shards.map(_.entityIds.size).sum == 0
+      }, Duration(35, TimeUnit.SECONDS), Duration(2, TimeUnit.SECONDS))
     }
   }
 
 }
 
-case class TestMessage(msg: String, replyTo: ActorRef)
+case class TestMessage(msg: String)
 case class TestReply(msg: String)
 
 class TestActor extends Actor {
   override def receive = {
-    case TestMessage(msg, replyTo) => {
-      System.out.println("Received TestMessage")
-      replyTo ! TestReply(msg)
-    }
+    case TestMessage(msg) =>
+      sender() ! TestReply(msg)
+
   }
 }

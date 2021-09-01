196e0690a276a4e1d02d2d2b3ffab102282a8ef8
Rajesh Kalaria <rajesh.kalaria@evernym.com>
2021-09-01 10:17:21 +0530

    fix unhandled event causing app state change

---
+++ verity/src/test/scala/com/evernym/verity/actor/persistence/recovery/latest/verity2/vas/UnhandledEventRecoverySpec.scala
@@ -0,0 +1,55 @@
+package com.evernym.verity.actor.persistence.recovery.latest.verity2.vas
+
+import akka.actor.{Actor, ActorRef, Props}
+import com.evernym.verity.actor.SignedUp
+import com.evernym.verity.actor.appStateManager.AppStateEvent
+import com.evernym.verity.actor.persistence.recovery.base.BaseRecoveryActorSpec
+import com.evernym.verity.actor.persistence.GetPersistentActorDetail
+import com.evernym.verity.util2.ExecutionContextProvider
+
+import scala.concurrent.ExecutionContext
+import scala.concurrent.duration._
+
+class UnhandledEventRecoverySpec
+  extends BaseRecoveryActorSpec
+    with AgencyAgentEventSetter
+    with UserAgentEventSetter {
+
+  override def beforeAll(): Unit = {
+    super.beforeAll()
+    setupBasicAgencyAgent()
+    setupBasicUserAgent()
+    addEventsToPersistentStorage(mySelfRelAgentPersistenceId, scala.collection.immutable.Seq(SignedUp()))
+    closeClientWallets(Set(myAgencyAgentEntityId, mySelfRelAgentEntityId))
+  }
+
+  "User agent actor" - {
+    "when started with an unhandled event" - {
+      "should not change app state" in {
+        system.actorOf(MockAppStateListener.props(self))
+        uaRegion ! GetPersistentActorDetail
+        expectNoMessage(5.seconds)
+      }
+    }
+  }
+
+  lazy val ecp: ExecutionContextProvider = new ExecutionContextProvider(appConfig)
+  override def executionContextProvider: ExecutionContextProvider = ecp
+  override def futureWalletExecutionContext: ExecutionContext = ecp.walletFutureExecutionContext
+}
+
+object MockAppStateListener {
+  def props(replyTo: ActorRef): Props = Props(new MockAppStateListener(replyTo))
+}
+
+class MockAppStateListener(replyTo: ActorRef)
+  extends Actor {
+
+  override def preStart(): Unit = {
+    context.system.eventStream.subscribe(self, classOf[AppStateEvent])
+  }
+
+  override def receive: Receive = {
+    case ase: AppStateEvent => replyTo ! ase
+  }
+}

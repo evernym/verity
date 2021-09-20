39ae3c56a6d4f33e20813e480fab1be048f6b326
Rajesh Kalaria <rajesh.kalaria@evernym.com>
2021-09-20 10:23:51 +0530

    continuous refactoring

--- verity/src/main/scala/com/evernym/verity/actor/agent/HasAgentActivity.scala
+++ verity/src/main/scala/com/evernym/verity/actor/agent/HasAgentActivity.scala
@@ -3,7 +3,7 @@ package com.evernym.verity.actor.agent
 import akka.actor.{ActorRef, ActorSystem}
 import akka.cluster.sharding.ClusterSharding
 import com.evernym.verity.actor.ForIdentifier
-import com.evernym.verity.actor.metrics.{ActivityTracking, ActivityWindow, AgentActivity}
+import com.evernym.verity.actor.metrics.{ActivityTrackingCommand, ActivityWindow, AgentActivity}
 import com.evernym.verity.config.{AppConfig, ConfigUtil}
 import com.evernym.verity.constants.ActorNameConstants.ACTIVITY_TRACKER_REGION_ACTOR_NAME
 import com.evernym.verity.observability.metrics.CustomMetrics.AS_NEW_USER_AGENT_COUNT
@@ -22,20 +22,21 @@ trait HasAgentActivity {
     lazy val activityTrackerRegionName: String = ACTIVITY_TRACKER_REGION_ACTOR_NAME
     lazy val activityTrackerRegion: ActorRef = ClusterSharding(system).shardRegion(activityTrackerRegionName)
 
-    private def sendToRegion(id: DomainId, msg: ActivityTracking): Unit =
+    private def sendToRegion(id: DomainId, msg: ActivityTrackingCommand): Unit =
       activityTrackerRegion ! ForIdentifier(id, msg)
 
     def track(msgType: String,
               domainId: DomainId,
               relId: Option[String]=None,
               timestamp: String=TimeUtil.nowDateString) : Unit = {
-          sendToRegion(
-            domainId,
-            AgentActivity(domainId, timestamp, msgType, relId)
-          )
+      sendToRegion(
+        domainId,
+        AgentActivity(domainId, timestamp, msgType, relId)
+      )
     }
 
     def newAgent(sponsorRel: Option[SponsorRel], metricsWriter: MetricsWriter): Unit = {
+      //TODO: why we are just recording metrics, but not the activity itself (in ActivityTracker)?
       val tags = sponsorRel.map(s => ConfigUtil.getSponsorRelTag(appConfig, s)).getOrElse(Map())
       metricsWriter.gaugeIncrement(AS_NEW_USER_AGENT_COUNT, tags = tags)
     }

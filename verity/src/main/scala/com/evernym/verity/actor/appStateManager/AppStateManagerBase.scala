22a483ce73f4f4d0210ca0e2f29ec84fe0968b39
Rajesh Kalaria <rajesh.kalaria@evernym.com>
2021-10-19 20:13:18 +0530

    add TODO

--- verity/src/main/scala/com/evernym/verity/actor/appStateManager/AppStateManagerBase.scala
+++ verity/src/main/scala/com/evernym/verity/actor/appStateManager/AppStateManagerBase.scala
@@ -243,7 +243,7 @@ trait AppStateManagerBase extends HasExecutionContextProvider { this: Actor =>
             .system
             .terminate()
             .map { _ =>
-              cluster.down(cluster.selfAddress)
+              cluster.down(cluster.selfAddress)   //TODO: need to confirm if this is required or not
               val terminationFinishTime = LocalDateTime.now()
               val timeTaken = ChronoUnit.MILLIS.between(terminationStartTime, terminationFinishTime)
               logger.info(s"coordinated shutdown finished in millis: $timeTaken")

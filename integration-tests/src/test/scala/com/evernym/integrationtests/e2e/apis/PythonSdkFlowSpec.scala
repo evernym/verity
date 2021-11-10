3a0b095d6f54073ea02bc949ac43ce67841ba72c
Matthew Spence <Matthew.Spence@evernym.com>
2021-11-10 10:22:39 -0600

    update flow spec versions

--- integration-tests/src/test/scala/com/evernym/integrationtests/e2e/apis/PythonSdkFlowSpec.scala
+++ integration-tests/src/test/scala/com/evernym/integrationtests/e2e/apis/PythonSdkFlowSpec.scala
@@ -6,5 +6,5 @@ import com.evernym.integrationtests.e2e.tag.annotation.Integration
 @Integration
 class PythonSdkFlowSpec extends SdkFlowSpec {
   override def specifySdkType(env: IntegrationTestEnv): IntegrationTestEnv =
-    SdkFlowSpec.specifySdkForType("python", "0.5.0", env)
+    SdkFlowSpec.specifySdkForType("python", "0.5.0+5349ae4e", env)
 }

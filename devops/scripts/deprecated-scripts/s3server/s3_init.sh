a8193a5ef054fdef6e09866b08669398f8ca1fc7
LazarevY <yaroslav.lazarev@evernym.com>
2022-01-18 17:55:23 +0300

    VE-3279: make add bucket name to endpoint-url env variables

--- devops/scripts/deprecated-scripts/s3server/s3_init.sh
+++ devops/scripts/deprecated-scripts/s3server/s3_init.sh
@@ -5,7 +5,7 @@ CUR_DIR=$( realpath $(dirname $0) )
 PROJ_ROOT=$( realpath "$CUR_DIR" )
 PROJ_ROOT=$( realpath ${PROJ_ROOT}/../../.. )
 BLOB_BUCKET=${BLOB_BUCKET:-blob-bucket}
-DEFAULT_ENDPOINT="http://localhost:8001"
+DEFAULT_ENDPOINT="http://${BLOB_BUCKET}.localhost:8001"
 BLOB_S3_ENDPOINT="${BLOB_S3_ENDPOINT:-$DEFAULT_ENDPOINT}"
 if [ ! -d "${PROJ_ROOT}/py_modules" ] ; then
     echo "Downloading boto3..."

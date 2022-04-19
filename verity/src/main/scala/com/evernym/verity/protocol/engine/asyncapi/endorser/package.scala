af812d1ecf5f20b545665112df63aed449253b68
Darko Kulic <darko.kulic@evernym.com>
2022-04-19 16:22:09 +0200

    Modify configuration for team1

--- verity/src/main/scala/com/evernym/verity/protocol/engine/asyncapi/endorser/package.scala
+++ verity/src/main/scala/com/evernym/verity/protocol/engine/asyncapi/endorser/package.scala
@@ -3,7 +3,7 @@ package com.evernym.verity.protocol.engine.asyncapi
 package object endorser {
   val VDR_TYPE_INDY = "indy"
 
-  val INDY_LEDGER_PREFIX = "did:sov"
+  val INDY_LEDGER_PREFIX = "did:indy:sovrin:builder"
   val SUPPORTED_LEDGER_PREFIXES = List(INDY_LEDGER_PREFIX)
 
   val ENDORSEMENT_RESULT_SUCCESS_CODE = "ENDT-RES-0001"

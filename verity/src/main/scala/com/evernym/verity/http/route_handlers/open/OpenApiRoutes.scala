f99db42f5b728bd518d3e381c9c6d02ec30c5f71
Rajesh Kalaria <rajesh.kalaria@evernym.com>
2022-05-16 14:01:11 +0530

    fix 'healthCheckRouteV2' route access issue, make it open instead restricted

--- verity/src/main/scala/com/evernym/verity/http/route_handlers/open/OpenApiRoutes.scala
+++ verity/src/main/scala/com/evernym/verity/http/route_handlers/open/OpenApiRoutes.scala
@@ -2,7 +2,7 @@ package com.evernym.verity.http.route_handlers.open
 
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.Route
-import com.evernym.verity.http.route_handlers.{PlatformServiceProvider, PlatformWithExecutor}
+import com.evernym.verity.http.route_handlers.PlatformWithExecutor
 
 /**
  * api routes which are public/open for everyone
@@ -12,8 +12,9 @@ trait OpenApiRoutes
   extends PackedMsgEndpointHandler
     with RestApiEndpointHandler
     with GetInviteRestEndpointHandler
-    with HeartbeatEndpointHandler {
+    with HeartbeatEndpointHandler
+    with HealthCheckEndpointHandlerV2 {
   this: PlatformWithExecutor =>
 
-  protected val openApiRoutes: Route = packedMsgRoute ~ restRoutes ~ getInviteRoute ~ heartbeatRoute
+  protected val openApiRoutes: Route = packedMsgRoute ~ restRoutes ~ getInviteRoute ~ heartbeatRoute ~ healthCheckRouteV2
 }
